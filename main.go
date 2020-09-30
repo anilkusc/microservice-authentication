8118c4e8822565fed0ca746ca9dba02062d38616
anilkusc <anilkuscu95@hotmail.com>
2020-09-30 16:20:44 +0300

    added logging feature

--- main.go
+++ main.go
@@ -16,10 +16,10 @@ var (
 func main() {
 
 	r := mux.NewRouter()
-	r.HandleFunc("/index", Index).Methods("POST")
-	r.HandleFunc("/login", Login).Methods("POST")
-	r.HandleFunc("/logout", Logout).Methods("POST")
-	r.HandleFunc("/auto", AutoLogin).Methods("POST")
+	r.HandleFunc("/index", Logger(Index)).Methods("POST")
+	r.HandleFunc("/login", Logger(Login)).Methods("POST")
+	r.HandleFunc("/logout", Logger(Logout)).Methods("POST")
+	r.HandleFunc("/auto", Logger(AutoLogin)).Methods("POST")
 	fmt.Println("Serving on:8080")
 	http.ListenAndServe(":8080", r)
 }
---
+++ middlewares.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestLog := "{'client':" + r.RemoteAddr + ",'server':" + r.URL.Host + r.URL.Path + ",'client-agent':" + r.Header.Get("User-Agent") + ",'method':" + r.Method + "}"
+		log.Println(requestLog)
+		next(w, r)
+	}
+
+}
